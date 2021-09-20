f0e586248bf508dd07cd43f029bcc1ceb95c5f9e
a127807 <pete_fittante@progressive.com>
2021-09-20 08:33:35 -0400

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using App.Repository;
+using App.Repository.ApiClient;
+using System;
+using MilSiteCore.Models;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+HttpClient httpClient = new();
+IWebApiExecuter apiExectuter = new WebApiExecuter("http://localhost/", httpClient);
+
+await GetLocations();
+
+async Task GetLocations()
+{
+	LocationRepository repository = new(apiExectuter);
+
+	var locations = await repository.GetAsync();
+	foreach (var location in locations)
+	{
+		Console.WriteLine($"Locations: {location.LocName}");
+	}
+}
